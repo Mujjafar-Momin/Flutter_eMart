a70ea0f3e638c507dd2969e641ce871193c73ba3
Mujjafar_07 <mujjafarmomin@nimapinfotech.com>
2023-09-07 17:38:46 +0530

    item details screen added

--- lib/views/category_screen/item_details.dart
+++ lib/views/category_screen/item_details.dart
@@ -19,166 +19,159 @@ class ItemDetails extends StatelessWidget {
               icon: const Icon(Icons.favorite_border_outlined))
         ],
       ),
-      body: Column(
-        children: [
-          SingleChildScrollView(
-            scrollDirection: Axis.vertical,
+      body: Column(children: [
+        Expanded(
+            child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SingleChildScrollView(
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                //swiper
+                VxSwiper.builder(
+                    autoPlay: true,
+                    aspectRatio: 16 / 9,
+                    height: 350,
+                    itemCount: 4,
+                    itemBuilder: (context, index) {
+                      return Image.asset(
+                        imgFc5,
+                        width: double.infinity,
+                        fit: BoxFit.cover,
+                      );
+                    }),
+                10.heightBox,
+                title!.text
+                    .fontFamily(bold)
+                    .color(darkFontGrey)
+                    .size(18)
+                    .make(),
+                10.heightBox,
+                VxRating(
+                  onRatingUpdate: (value) {},
+                  count: 5,
+                  selectionColor: golden,
+                  stepInt: true,
+                  size: 25,
+                ),
+                10.heightBox,
+                "\$30,000"
+                    .text
+                    .fontFamily(bold)
+                    .size(18)
+                    .color(redColor)
+                    .make(),
+                10.heightBox,
+                Row(
                   children: [
-                    //swiper
-                    VxSwiper.builder(
-                        autoPlay: true,
-                        aspectRatio: 16 / 9,
-                        height: 350,
-                        itemCount: 4,
-                        itemBuilder: (context, index) {
-                          return Image.asset(
-                            imgFc5,
-                            width: double.infinity,
-                            fit: BoxFit.cover,
-                          );
-                        }),
-                    10.heightBox,
-                    title!.text
-                        .fontFamily(bold)
-                        .color(darkFontGrey)
-                        .size(18)
-                        .make(),
-                    10.heightBox,
-                    VxRating(
-                      onRatingUpdate: (value) {},
-                      count: 5,
-                      selectionColor: golden,
-                      stepInt: true,
-                      size: 25,
-                    ),
-                    10.heightBox,
-                    "\$30,000"
-                        .text
-                        .fontFamily(bold)
-                        .size(18)
-                        .color(redColor)
-                        .make(),
-                    //Seller
+                    Expanded(
+                        child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        "Seller".text.white.fontFamily(semibold).make(),
+                        5.heightBox,
+                        "In House Products"
+                            .text
+                            .color(darkFontGrey)
+                            .fontFamily(semibold)
+                            .make()
+                      ],
+                    )),
+                    const CircleAvatar(
+                      backgroundColor: whiteColor,
+                      child: Icon(
+                        Icons.message_rounded,
+                        color: darkFontGrey,
+                      ),
+                    )
+                  ],
+                )
+                    .box
+                    .height(60)
+                    .padding(const EdgeInsets.symmetric(horizontal: 16))
+                    .color(textfieldGrey)
+                    .make(),
+                Column(
+                  children: [
                     Row(
                       children: [
-                        Expanded(
-                            child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            "Seller".text.white.fontFamily(semibold).make(),
-                            5.heightBox,
-                            "In House Products"
-                                .text
-                                .color(darkFontGrey)
-                                .fontFamily(semibold)
-                                .make()
-                          ],
-                        )),
-                        const CircleAvatar(
-                          backgroundColor: whiteColor,
-                          child: Icon(
-                            Icons.message_rounded,
-                            color: darkFontGrey,
-                          ),
+                        SizedBox(
+                          width: 100,
+                          child: "Color: ".text.make(),
+                        ),
+                        Row(
+                          children: List.generate(
+                              3,
+                              (index) => VxBox()
+                                  .size(40, 40)
+                                  .margin(const EdgeInsets.all(8))
+                                  .color(Vx.randomPrimaryColor)
+                                  .roundedFull
+                                  .make()),
                         )
                       ],
-                    )
-                        .box
-                        .color(textfieldGrey)
-                        .height(60)
-                        .padding(const EdgeInsets.symmetric(horizontal: 16))
-                        .make(),
-                
-                    //color section
+                    ).box.padding(const EdgeInsets.all(8)).make(),
                     10.heightBox,
-                    Column(
+                     Row(
                       children: [
-                        Expanded(
-                          child: Row(
-                            children: [
-                              SizedBox(
-                                width: 100,
-                                child: "Color: ".text.make(),
-                              ),
-                              Row(
-                                children: List.generate(
-                                    3,
-                                    (index) => VxBox()
-                                        .size(40, 40)
-                                        .margin(const EdgeInsets.all(8))
-                                        .color(Vx.randomPrimaryColor)
-                                        .roundedFull
-                                        .make()),
-                              )
-                            ],
-                          ).box.padding(const EdgeInsets.all(8)).make(),
+                        SizedBox(
+                          width: 100,
+                          child: "Color: ".text.make(),
                         ),
-                
-                        //quntity row 
-                        Expanded(
-                          child: Row(
-                            children: [
-                              SizedBox(
-                                width: 100,
-                                child: "Color: ".text.make(),
-                              ),
-                              Expanded(
-                                child: Row(
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  children: [
-                                   IconButton(onPressed: (){}, icon: Icon(Icons.remove)),
-                                   "0".text.size(16).color(darkFontGrey).fontFamily(bold).make(),
-                                    IconButton(onPressed: (){}, icon: Icon(Icons.add)),
-                                  ],
-                                  ),
-                              )
-                            ],
-                          ).box.padding(const EdgeInsets.all(8)).make(),
-                        ),
-                    Row(
+                        Row(
                           children: [
-                            SizedBox(
-                              width: 100,
-                              child: "Color: ".text.make(),
-                            ),
-                            Expanded(
-                              child: Row(
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                 IconButton(onPressed: (){}, icon: Icon(Icons.remove)),
+                              IconButton(onPressed: (){}, icon: const Icon(Icons.remove)),
                                  "0".text.size(16).color(darkFontGrey).fontFamily(bold).make(),
-                                  IconButton(onPressed: (){}, icon: Icon(Icons.add)),
-                                ],
-                                ),
-                            )
-                          ],
-                        ).box.padding(const EdgeInsets.all(8)).make()
-                     ],
-                    ).box.white.shadowSm.make(),
-                  ],
-                ),
-              )
-            ]),
+                                IconButton(onPressed: (){}, icon: const Icon(Icons.add)),
+                                 "0 available".text.size(16).color(textfieldGrey).fontFamily(bold).make(),
+                          ]
+                        )
+                      ],
+                    ).box.padding(const EdgeInsets.all(8)).make(),  
+                 Row(
+                      children: [
+                        SizedBox(
+                          width: 100,
+                          child: "Total: ".text.make(),
+                        ),
+                        "\$0.00".text.color(redColor).size(16).fontFamily(bold).make()
+                      ],
+                    ).box.padding(const EdgeInsets.all(8)).make()
+                 ],
+                ).box.white.shadowSm.make(),
+
+                10.heightBox,
+                "Description".text.color(darkFontGrey).fontFamily(semibold).make(),
+          
+                10.heightBox,
+                "This is dummy item and dummy description here...".text.color(darkFontGrey).fontFamily(semibold).make(),
+                ListView(
+                  shrinkWrap: true,
+                  children: List.generate(5, (index) => ListTile(
+                  title:  "Video".text.color(darkFontGrey).fontFamily(semibold).make(),
+                  trailing:const Icon(Icons.arrow_forward),
+                )),)
+
+
+
+
+              ],
+            ),
           ),
-          SizedBox(
-            width: double.infinity,
-            height: 60,
-            child: commonButton(
-                onPressed: () {},
-                color: redColor,
-                textColor: whiteColor,
-                title: "Add to cart"),
-          ),
-          // 10.heightBox
-        ],
-      ),
+        )),
+        SizedBox(
+          width: double.infinity,
+          height: 60,
+          child: commonButton(
+              onPressed: () {},
+              color: redColor,
+              textColor: whiteColor,
+              title: "Add to cart"),
+        ),
+      ]),
+
+      // 10.heightBox
     );
   }
 }
